d506dca9bee7a9451a29a350d299e1e4d74d0681
Artis3n <dev@quantummadness.com>
2019-08-19 16:17:06 -0400

    Configured Travis

--- molecule/default/tests/test_common.py
+++ molecule/default/tests/test_common.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -7,9 +8,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+@pytest.mark.parametrize('pkg', [
+    'bitwarden'
+])
+def test_installed(host, pkg):
+    package = host.package(pkg)
+    assert package.is_installed
